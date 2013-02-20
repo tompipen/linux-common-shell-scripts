ab19cabf6d47a01542a2530219ca4af116df26a3
Tzu-Chieh Lin <tompipen@gmail.com>
2013-02-21 02:26:58 +0800

    add node.js script

---
+++ install-nodejs.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+sudo apt-get -y install python-software-properties python g++ make
+sudo apt-get -y install software-properties-common
+sudo add-apt-repository -y ppa:chris-lea/node.js
+sudo apt-get -y update
+sudo apt-get -y install nodejs npm
