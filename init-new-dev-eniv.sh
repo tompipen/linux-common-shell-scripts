f878716742f02017bf32d88a4bba5bf9fe726a66
Tzu-Chieh Lin <tompipen@gmail.com>
2013-02-22 14:29:28 +0800

    Update init-new-dev-eniv.sh

--- init-new-dev-eniv.sh
+++ init-new-dev-eniv.sh
@@ -2,9 +2,9 @@
 sudo apt-get -y update
 sudo apt-get -y upgrade
 sudo apt-get -y install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev unzip
-sh generate-ssh.sh
-sh git-config.sh
-sh install-nodejs.sh
+bash generate-ssh.sh
+bash git-config.sh
+bash install-nodejs.sh
 \curl -L https://get.rvm.io | bash -s stable --ruby
 echo "gem: --no-ri --no-rdoc" > ~/.gemrc
 source ~/.rvm/scripts/rvm
