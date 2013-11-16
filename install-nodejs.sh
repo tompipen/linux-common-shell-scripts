e6bf786b206e41b245c8db5b88642d2988785e35
Tzu-Chieh Lin <tompipen@gmail.com>
2013-11-17 01:37:35 +0800

    update, remove npm

--- install-nodejs.sh
+++ install-nodejs.sh
@@ -3,4 +3,4 @@ sudo apt-get -y install python-software-properties python g++ make
 sudo apt-get -y install software-properties-common
 sudo add-apt-repository -y ppa:chris-lea/node.js
 sudo apt-get -y update
-sudo apt-get -y install nodejs npm
+sudo apt-get -y install nodejs
