722caea39e42dda767559bee2f1cce981f622400
Tzu-Chieh Lin <tompipen@gmail.com>
2014-04-10 22:12:36 +0800

    Update git-config.sh

--- git-config.sh
+++ git-config.sh
@@ -3,3 +3,4 @@ read -p "your name : " yourname
 read -p "your email : " youremail
 git config --global user.name $yourname
 git config --global user.email $youremail
+echo "export VISUAL=vim" > ~/.bash_profile
