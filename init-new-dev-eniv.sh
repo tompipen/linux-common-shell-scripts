52be1afa33fc0f9d520e8bbf36869dd0e466ca8c
Tzu-Chieh Lin <tompipen@gmail.com>
2014-04-10 19:13:37 +0800

    Update init-new-dev-eniv.sh

--- init-new-dev-eniv.sh
+++ init-new-dev-eniv.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 sudo apt-get -y update
 sudo apt-get -y upgrade
-sudo apt-get -y install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt-dev autoconf libc6-dev unzip gitg imagemagick
+sudo apt-get -y install build-essential bison openssl libreadline6 libreadline6-dev curl git-core zlib1g zlib1g-dev libssl-dev libyaml-dev libsqlite3-0 libsqlite3-dev sqlite3 libxml2-dev libxslt1-dev autoconf libc6-dev unzip gitg imagemagick
 bash generate-ssh.sh
 bash git-config.sh
 bash install-nodejs.sh
